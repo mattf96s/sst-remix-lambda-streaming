f72b89024378d9a469de796b795263928cd7fff4
Matthew Fainman <mattf96@gmail.com>
2023-07-31 23:58:15 +0200

    remove api gateway

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,39 +1,25 @@
 import { StackContext, Api, RemixSite } from "sst/constructs";
-import { Fn } from "aws-cdk-lib";
-import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
 
 export function API({ stack }: StackContext) {
-  const api = new Api(stack, "Api");
-
   const site = new RemixSite(stack, "Site", {
     path: "packages/my-remix-app/",
     runtime: "nodejs18.x",
+    nodejs: {
+      format: "esm",
+    },
     edge: false,
     timeout: 30,
+    memorySize: 512,
     waitForInvalidation: stack.stage === "prod",
     dev: {
       deploy: true,
     },
-    cdk: {
-      distribution: {
-        defaultBehavior: {
-          origin: new origins.HttpOrigin(Fn.parseDomainName(api.url)),
-        },
-      },
-    },
-  });
-
-  api.addRoutes(stack, {
-    "ANY /{proxy+}": {
-      type: "function",
-      cdk: {
-        function: site.cdk?.function,
-      },
+    environment: {
+      NODE_ENV: process.env.NODE_ENV || "development",
     },
   });
 
   stack.addOutputs({
-    ApiEndpoint: api.url,
     URL: site.url || "localhost",
   });
 }
